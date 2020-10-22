115c24637734bdf2bb38b97f1e894f6ce5e6a988
Pham Cao Dai <daikadai1026@gmail.com>
2020-10-22 16:53:33 +0700

    23. Product Details Reducer & Action

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -1,4 +1,4 @@
-import { PRODUCT_LIST_FAIL, PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS } from "../constants/productConstant";
+import { PRODUCT_DETAIL_FAIL, PRODUCT_DETAIL_REQUEST, PRODUCT_DETAIL_SUCCESS, PRODUCT_LIST_FAIL, PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS } from "../constants/productConstant";
 
 export const productListReducer = (state = { products: []}, action) => {
   switch(action.type) {
@@ -13,6 +13,28 @@ export const productListReducer = (state = { products: []}, action) => {
         products: action.payload
       }
     case PRODUCT_LIST_FAIL:
+      return {
+        loading: false,
+        error: action.payload
+      }
+    default:
+      return state
+  }
+}
+
+export const productDetailReducer = (state = { product: { reviews: []}}, action) => {
+  switch(action.type) {
+    case PRODUCT_DETAIL_REQUEST:
+      return {
+        loading: true,
+        ...state
+      }
+    case PRODUCT_DETAIL_SUCCESS:
+      return {
+        loading: false,
+        product: action.payload
+      }
+    case PRODUCT_DETAIL_FAIL:
       return {
         loading: false,
         error: action.payload
