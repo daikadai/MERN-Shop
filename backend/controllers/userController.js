d57898f598309eb03ecf7ade80ef0f1c89efaa8e
Pham Cao Dai <daikadai1026@gmail.com>
2020-10-23 15:26:29 +0700

    32. Custom Authentication Middleware

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -24,6 +24,26 @@ const authUser = expressAsyncHandler(async(req,res) => {
   }
 })
 
+// @desc    Get user profile
+// @route   POST /api/users/profile
+// @access  Public
+const getUserProfile = expressAsyncHandler(async (req,res) => {
+  const user = await User.findById(req.user._id)
+
+  if(user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    })
+  } else {
+    res.status(404)
+    throw new Error('User not found')
+  }
+})
+
 export {
-  authUser
+  authUser,
+  getUserProfile
 }
