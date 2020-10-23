94f40d576fc7b98bf2eff2986019925512654c6c
Pham Cao Dai <daikadai1026@gmail.com>
2020-10-23 12:35:54 +0700

    30. User Authentication Endpoint

--- backend/data/users.js
+++ backend/data/users.js
@@ -5,7 +5,7 @@ const users = [
     name: 'Admin User',
     email: 'admin@example.com',
     password: bcrypt.hashSync('123123', 10),
-    idAdmin: true
+    isAdmin: true
   },
   {
     name: 'John Doe',
