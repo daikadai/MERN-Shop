f6669db4ca41b9f198d68537f481066fe90ec261
Pham Cao Dai <daikadai1026@gmail.com>
2020-11-14 18:24:42 +0700

    64. Create & Update Product Endpoints

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -27,7 +27,7 @@ const getProductById = expressAsyncHandler(async(req,res) => {
 
 // @desc    Delete a product
 // @route   DELETE /api/products/:id
-// @access  Public/Admin
+// @access  Private/Admin
 const deleteProduct = expressAsyncHandler(async(req,res) => {
   const product = await Product.findById(req.params.id)
 
@@ -39,8 +39,56 @@ const deleteProduct = expressAsyncHandler(async(req,res) => {
     throw new Error('Product not found')
   }
 })
+
+// @desc    Create a product
+// @route   POST /api/products
+// @access  Private/Admin
+const createProduct = expressAsyncHandler(async(req,res) => {
+  const product = new Product({
+    name: 'Sample name',
+    price: 0,
+    user: req.user._id,
+    image: '/image/sample.jpg',
+    brand: 'Sample brand',
+    category: 'Sample category',
+    countInStock: 0,
+    numReviews: 0,
+    description: 'Sample description'
+  })
+
+  const createdProduct = await product.save()
+  res.status(201).json(createdProduct)
+})
+
+// @desc    Update a product
+// @route   PUT /api/products/:id
+// @access  Private/Admin
+const updateProduct = expressAsyncHandler(async(req,res) => {
+  const { name, price, description, image, brand, category, countInStock } = req.body
+
+  const product = await Product.findById(req.params.id)
+
+  if(product) {
+    product.name = name
+    product.price = price
+    product.description = description
+    product.image = image
+    product.brand = brand
+    product.category = category
+    product.countInStock = countInStock
+
+    const updatedProduct = await product.save()
+    res.json(updatedProduct)
+  } else {  
+    res.status(404)
+    throw new Error('Product Not Found')
+  }
+  
+})
 export {
   getProducts,
   getProductById,
-  deleteProduct
+  deleteProduct,
+  createProduct,
+  updateProduct
 }
